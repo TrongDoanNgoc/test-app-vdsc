b1456a7d8593c2e1b900b2375d64aa624a07c220
TrongDoanNgoc <trong.doanngoc2023@gmail.com>
2025-10-27 16:12:27 +0700

    refactor: restructure project by moving App component to src directory and removing old App.tsx

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src/'],
+        extensions: ['.js', '.ts', '.tsx', '.json', '.jsx'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+    'react-native-worklets/plugin',
+  ],
 };
